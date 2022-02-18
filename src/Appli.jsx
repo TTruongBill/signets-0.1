92514ba0f6059bb95183b8ba09d1414df0913c82
Tien Tien Truong <tientientruong@live.fr>
2022-02-17 19:41:20 -0500

    ajout d'un dossier json

--- src/Appli.jsx
+++ src/Appli.jsx
@@ -1,9 +1,10 @@
 import './Appli.scss';
+import Entete from './Entete';
 
 export default function Appli() {
   return (
     <div className="Appli">
-        Gabarit de base d'une Application React
+              <Entete />
     </div>
   );
 }
---
+++ src/Entete.jsx
@@ -0,0 +1,15 @@
+import "./Entete.scss";
+
+export default function Entete() {
+    return (
+        <header className='Entete'>
+            <h1>Signets</h1>
+            <nav>
+            <a href="#">Contactez-nous</a>
+            <a href="#">
+            logo
+            </a>
+            </nav>
+        </header>
+    );
+}
